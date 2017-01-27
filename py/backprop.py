6f6def9ce77bb0ff8405e6f38529e5e91d1844f8
Jeff Sember <jpsember@gmail.com>
2017-01-27 14:13:07 -0800

    Issue #8: spiral function

--- py/backprop.py
+++ py/backprop.py
@@ -7,12 +7,23 @@ import numpy as np
 import math
 from func import *
 
-
+def build_spiral_data(points_per_class = 100, num_classes = 3):
+  X = np.zeros((points_per_class*num_classes,2)) # data matrix (each row = single example)
+  y = np.zeros(points_per_class*num_classes, dtype='uint8') # class labels
+  for j in xrange(num_classes):
+    ix = range(points_per_class*j,points_per_class*(j+1))
+    r = np.linspace(0.0,1,points_per_class) # radius
+    t = np.linspace(j*4,(j+1)*4,points_per_class) + np.random.randn(points_per_class)*0.2 # theta
+    X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+    y[ix] = j
+  return (X,y)
 
 if True:
   warning("trying out plot")
-  plt.plot([1,2,3,4])
-  plt.ylabel('some numbers')
+
+  X,y = build_spiral_data()
+
+  plt.scatter(X[:, 0], X[:, 1], c=y, s=10, cmap=plt.cm.winter)
   save_plot()
   sys.exit()
 
