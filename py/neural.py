61a167b08ad4eb3e7f7e1bc6e4d39f685751261c
Jeff Sember <jpsember@gmail.com>
2017-01-30 12:14:15 -0800

    Issue #13: adding new script for numpy/matrix representation

---
+++ py/neural.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+# Represent a neural network using numpy;
+# train it to assign colors to the 'spiral' points
+#
+
+import numpy as np
+import math
+
+
+class App:
+
+
+  def run(self):
+    np.random.seed(1965)
+
+
+
+App().run()
