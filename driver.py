6527ad04436948daf542166bdb97c17e021974b8
Jeff Sember <jpsember@gmail.com>
2024-09-19 15:30:34 -0700

    Issue #15: even the 'normal' (non-micro) project seems to train in a reasonable time

--- driver.py
+++ driver.py
@@ -10,9 +10,11 @@ print(platform.python_version())
 
 
 from pycore.base import *
-pr("====== using classifier_micro here, until I figure out a better way")
-from example_classifier_micro.classifier_train import ClassifierTrain
-from example_classifier_micro.classifier_inference import ClassifierInference
+
+pr("The project is HARD CODED here, which is error prone")
+
+from example_classifier.classifier_train import ClassifierTrain
+from example_classifier.classifier_inference import ClassifierInference
 from example_yolo.yolo_train import YoloTrain
 from example_yolo.yolo_inference import YoloInference
 
