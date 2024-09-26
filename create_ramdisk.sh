4abc552ba1ca6c8f07a15030204a7427d5fc7a81
Jeff Sember <jpsember@gmail.com>
2024-09-26 12:38:53 -0700

    Issue #17

--- create_ramdisk.sh
+++ create_ramdisk.sh
@@ -4,8 +4,10 @@ set -eu
 
 echo "Creating ram disk"
 
+DISKNAME=ml_disk
+
 # size is (# megabytes * 2048), so for 1 Gb (1024 Mb), we use 1024 * 2048 = 2097152
 #
-diskutil erasevolume HFS+ "ml_disk" `hdiutil attach -nomount ram://2097152`
+diskutil erasevolume HFS+ "${DISKNAME}" `hdiutil attach -nomount ram://2097152`
 
-echo "To delete the ram disk, type 'umount ml_disk'"
+echo "To delete the ram disk, type 'hdiutil eject ${DISKNAME}'"
